324d0c39b01dd4a280dc758e4dddb2d4fb67f0e7
RogelioElMero <rogelio_1322134109@uptecamac.edu.mx>
2023-02-09 20:43:45 -0600

    guardar trasporte

--- main.js
+++ main.js
@@ -5,15 +5,15 @@ var Servicios=[];
 var Empleados=[];
 
 GuardarTrasporte = function(){
-    let Modelo = document.getElementById("Modelo").value;
+    let Modelo = document.getElementById('Modelo').value;
     let Suspencion= document.getElementById("Suspencion").value;
     let Capacidad=document.getElementById("Capacidad").value;
     let Placa=document.getElementById("Placa").value;
     let NumeroSerie=document.getElementById("NumeroSerie").value;
     let Tracto=document.getElementById("Tracto").value;
-  let Trasporte = new Trasporte(Modelo,Suspencion,Capacidad,Placa,NumeroSerie,Tracto);
-    Trasportes.push(Trasporte);
-
+  let Trs = new Trasporte(Modelo,Suspencion,Capacidad,Placa,NumeroSerie,Tracto);
+    Trasportes.push(Trs);
+    alert("Veiculo guardado exitosamente");
 }
 
 
